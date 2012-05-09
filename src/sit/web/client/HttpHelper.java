25562043e9bdb4ae7cf4525f5c9068d7f36a98d4
schuller <schuller@AHS-821.iao.fraunhofer.de>
2012-05-09 14:31:17 +0200

    light version of sitjar deleted several unused (on mobile) packages changed several files for compability with android 2.2

--- src/sit/web/client/HttpHelper.java
+++ src/sit/web/client/HttpHelper.java
@@ -54,17 +54,14 @@ public class HttpHelper {
     private TrustManager[] trustAllCerts = new TrustManager[]{
         new X509TrustManager() {
 
-            @Override
             public java.security.cert.X509Certificate[] getAcceptedIssuers() {
                 return null;
             }
 
-            @Override
             public void checkClientTrusted(
                     java.security.cert.X509Certificate[] certs, String authType) {
             }
 
-            @Override
             public void checkServerTrusted(
                     java.security.cert.X509Certificate[] certs, String authType) {
             }
@@ -83,7 +80,6 @@ public class HttpHelper {
 
         HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
 
-            @Override
             public boolean verify(String string, SSLSession ssls) {
                 return true;
             }
