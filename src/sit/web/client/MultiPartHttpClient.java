d2ff1e5562192480b8b70ea53e5e3c45df5dff7d
simon <simon.thiel@gmx.de>
2012-06-21 16:53:58 +0200

    working on Multipart client

---
+++ src/sit/web/client/MultiPartHttpClient.java
@@ -0,0 +1,16 @@
+/*
+ *  Description of MultiPartHttpClient
+ * 
+ *  @author Simon Thiel
+ *  @version $Revision: $
+ *  @date 21.06.2012
+ */
+package sit.web.client;
+
+/**
+ * MultiPartHttpClient
+ * 
+ */
+public class MultiPartHttpClient {
+
+}
