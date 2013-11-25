540b6799bf4e1a1ecc180b1c510bcf94458cf318
Simon <simon.thiel@gmx.de>
2013-11-25 09:42:38 +0100

    added download function in HttpHelper

--- src/sit/web/client/HttpHelper.java
+++ src/sit/web/client/HttpHelper.java
@@ -1,28 +1,30 @@
 /*
-* Copyright 2013 Simon Thiel
-*
-* This file is part of SitJar.
-*
-* SitJar is free software: you can redistribute it and/or modify
-* it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-* 
-* SitJar is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Lesser General Public License for more details.
-* 
-* You should have received a copy of the GNU Lesser General Public License
-* along with SitJar. If not, see <http://www.gnu.org/licenses/lgpl.txt>.
-*/
-
+ * Copyright 2013 Simon Thiel
+ *
+ * This file is part of SitJar.
+ *
+ * SitJar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SitJar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SitJar. If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+ */
 package sit.web.client;
 
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FilterOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
@@ -54,6 +56,8 @@ import sit.web.multipart.MultipartContainer;
 public class HttpHelper {
 
     private final static boolean USE_APACHE = true;
+    
+    private static final int BUFFER_SIZE = 1024*512;
 
     public static String getBase64UserNamePwdToken(String username, String password) {
         return Base64.encodeBytes((username + ":" + password).getBytes());
@@ -99,7 +103,6 @@ public class HttpHelper {
         return new URL(myUrl);
     }
 
-   
     public HttpHelper() {
         HTTPTrustHelper.initAllTrustingManager("TLS"); //or "SSL"
     }
@@ -187,14 +190,14 @@ public class HttpHelper {
         if (charSet != null) {
             contentType += HttpConstants.SUB_FIELD_SEPARATOR + HttpConstants.CHARSET_CONTENT_TYPE_TAG + charSet.name(); //text/html; charset=utf-8
             //##CHARSET_MARKER##            
-        }else{
+        } else {
             charSet = Charset.defaultCharset();
         }
-        
+
         try {
             return doHTTPRequest(method, host, port, path, payload.getBytes(charSet), contentType, isHTTPS, unamePword64);
         } catch (URISyntaxException ex) {
-            
+
             throw new MalformedURLException(ex.getMessage());
         }
 
@@ -219,14 +222,14 @@ public class HttpHelper {
     public HTTPResponse doHTTPRequest(String method, String host, int port, String path, byte[] payload, String contentType,
             boolean isHTTPS, String unamePword64) throws MalformedURLException, ProtocolException, IOException, URISyntaxException {
 
-       if (USE_APACHE){
-           return doApacheHTTPRequest(method, host, port, path, payload, contentType, isHTTPS, unamePword64);
-       }//else
-       
-       return doHttpURLConnectionRequest(method, host, port, path, payload, contentType, isHTTPS, unamePword64);
+        if (USE_APACHE) {
+            return doApacheHTTPRequest(method, host, port, path, payload, contentType, isHTTPS, unamePword64);
+        }//else
+
+        return doHttpURLConnectionRequest(method, host, port, path, payload, contentType, isHTTPS, unamePword64);
     }
-    
-      /**
+
+    /**
      *
      *
      * @param method
@@ -241,7 +244,7 @@ public class HttpHelper {
      * @throws MalformedURLException
      * @throws ProtocolException
      * @throws IOException
-     * @throws URISyntaxException  
+     * @throws URISyntaxException
      */
     public HTTPResponse doApacheHTTPRequest(String method, String host, int port, String path, byte[] payload, String contentType,
             boolean isHTTPS, String unamePword64) throws MalformedURLException, ProtocolException, IOException, URISyntaxException {
@@ -302,7 +305,42 @@ public class HttpHelper {
         httpclient.getConnectionManager().shutdown();
         return result;
     }
-   /**
+
+    private static boolean isHTTPS(String downloadUrl) {
+        return downloadUrl.startsWith("https");
+    }
+
+    public static void apacheDownload(String downloadUrl, File targetFile) throws IOException {
+
+        URL url = new URL(downloadUrl);
+
+        HttpClient httpclient;
+        if (isHTTPS(downloadUrl)) {
+            httpclient = HTTPTrustHelper.getNewHttpClient(Charset.defaultCharset(), (url.getPort() != -1) ? url.getPort() : url.getDefaultPort());
+        } else {
+            httpclient = new DefaultHttpClient();
+        }
+
+        HttpGet request = new HttpGet(downloadUrl);
+        HttpResponse response = httpclient.execute(request);
+
+        InputStream is = response.getEntity().getContent();
+        FileOutputStream writer = new FileOutputStream(targetFile);
+
+        try {
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bytesRead = 0;
+            while ((bytesRead = is.read(buffer, 0, buffer.length)) >= 0) {
+                writer.write(buffer, 0, bytesRead);
+            }
+        } finally {
+            writer.close();
+            is.close();
+            httpclient.getConnectionManager().shutdown();
+        }
+    }
+
+    /**
      *
      *
      * @param method
@@ -317,26 +355,26 @@ public class HttpHelper {
      * @throws MalformedURLException
      * @throws ProtocolException
      * @throws IOException
-     * @throws URISyntaxException  
+     * @throws URISyntaxException
      */
     public HTTPResponse doHttpURLConnectionRequest(String method, String host, int port, String path, byte[] payload, String contentType,
             boolean isHTTPS, String unamePword64) throws MalformedURLException, ProtocolException, IOException, URISyntaxException {
 
-        
+
         URL url = getURL(host, port, path, isHTTPS);
 
         return doHttpUrlConnectionRequest(url, method, contentType, payload, unamePword64);
     }
 
-    public HTTPResponse doHttpUrlConnectionRequest(URL url, String method, String contentType, byte [] payload, String unamePword64) throws MalformedURLException, ProtocolException, IOException, URISyntaxException{
-        
+    public HTTPResponse doHttpUrlConnectionRequest(URL url, String method, String contentType, byte[] payload, String unamePword64) throws MalformedURLException, ProtocolException, IOException, URISyntaxException {
+
         if (payload == null) { //make sure payload is initialized
             payload = new byte[0];
         }
 
         boolean isHTTPS = url.getProtocol().equalsIgnoreCase("https");
         HttpURLConnection connection;
-        
+
         if (isHTTPS) {
             connection = (HttpsURLConnection) url.openConnection();
         } else {
@@ -351,13 +389,12 @@ public class HttpHelper {
         if (isHTTPS) {
             connection.setRequestProperty("Authorization", "Basic " + unamePword64);
         }
-        
-        Logger.getLogger(HttpHelper.class.getName()).log(Level.FINER, "trying to connect:\n" + method + " " 
+
+        Logger.getLogger(HttpHelper.class.getName()).log(Level.FINER, "trying to connect:\n" + method + " "
                 + url + "\nhttps:" + isHTTPS
-                +"\nContentType:" + contentType
-                +"\nContent-Length:" + String.valueOf(payload.length)                
-                );
-        
+                + "\nContentType:" + contentType
+                + "\nContent-Length:" + String.valueOf(payload.length));
+
 
         connection.setDoInput(true);
         if (payload.length > 0) {
@@ -379,26 +416,25 @@ public class HttpHelper {
                 + response.message + " with code: " + response.code);
 
         if (response.code != 500) {
-            byte[] buffer =new byte[20480];
+            byte[] buffer = new byte[20480];
 
             ByteBuilder bytes = new ByteBuilder();
 
-            
+
             // get ready to read the response from the cgi script
             DataInputStream input = new DataInputStream(connection.getInputStream());
             boolean done = false;
-            while(!done){
+            while (!done) {
                 int readBytes = input.read(buffer);
-                done=(readBytes==-1);
+                done = (readBytes == -1);
 
-                if(!done){
-                    bytes.append(buffer,readBytes);
+                if (!done) {
+                    bytes.append(buffer, readBytes);
                 }
             }
             input.close();
-            response.reply=bytes.toString(Charset.defaultCharset());
+            response.reply = bytes.toString(Charset.defaultCharset());
         }
         return response;
-    }           
-    
+    }
 }
