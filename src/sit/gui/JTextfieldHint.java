4d3b2a2e78e6abc27ea06df11166b15738a1b25f
simon <simon.thiel@gmx.de>
2013-12-23 12:56:39 +0100

    FEATURE added JTextfieldHint

---
+++ src/sit/gui/JTextfieldHint.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2013 Simon Thiel
+ * 
+ * This file is part of SitJar.
+ * 
+ * SitJar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * SitJar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SitJar. If not, see <http://www.gnu.org/licenses/lgpl.txt>. * 
+ */
+package sit.gui;
+
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author simon
+ */
+public class JTextfieldHint {
+
+    public void addHint(JTextField textField, String hintText) {
+        registerEvents(textField, hintText);
+    }
+
+    private void registerEvents(final JTextField textField, final String hintText) {
+        textField.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) {
+                if (textField.getText().equals(hintText)) {
+                    textField.setText("");
+                }
+            }
+
+            public void focusLost(FocusEvent e) {
+                //do nothing
+            }
+        });
+    }
+}
